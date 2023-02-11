4e83a21ed7f32c84f33923d0a70f24ae7b8cd8ad
appinha <amanda_pinha@hotmail.com>
2023-02-11 17:26:22 -0300

    Adds testing

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  const renderComponent = () => render(<App />)
+
+  test('renders properly', () => {
+    const { container } = renderComponent();
+
+    expect(container).toHaveTextContent(/Student Database/i);
+    expect(container).toHaveTextContent(
+      /First Name(.*)Last Name(.*)Phone(.*)e-mail(.*)Primary Group(.*)Hours Studied/i
+    );
+    expect(container).toHaveTextContent(
+      /Joe(.*)Smith Jr(.*)781-633-2222(.*)something@somewhere.com(.*)Important People(.*)1000/i
+    );
+  });
+
+  test('initially renders sorted alphabetically by first Name', () => {
+    const { container } = renderComponent();
+
+    expect(container).toHaveTextContent(/Anna(.*)Joe/i);
+  })
 });
